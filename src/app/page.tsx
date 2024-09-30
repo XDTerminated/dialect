8f5ef39dcf88a9c776ecf1ecc6fae6ea0ddc58bc
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-09-29 19:41:11 -0500

    Fix: Got Rid of getData component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import GetData from "@/components/getData";
 import TranslatorBoxes from "@/components/TranslatorBoxes";
 import TitleBar from "@/components/TitleBar";
 
 export default function Home() {
     return (
         <>
-            {/* <Dropdown label={selectedLabel} items={dropdownItems} onSelect={(label: string) => setSelectedLabel(label)} /> */}
             <TitleBar />
             <TranslatorBoxes />
-            <GetData />
         </>
     );
 }
